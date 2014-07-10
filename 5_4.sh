80e491b44e92456d2aaf8728071d07daa08b6da9
whilgeek <whilgeek@gmail.com>
2014-07-10 10:20:15 +0800

    init commit

---
+++ 5_4.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+mkdir -v ../binutils-build
+cd ../binutils-build
+../binutils-2.24/configure     \
+    --prefix=/tools            \
+    --with-sysroot=$LFS        \
+    --with-lib-path=/tools/lib \
+    --target=$LFS_TGT          \
+    --disable-nls              \
+    --disable-werror
+make
+case $(uname -m) in
+  x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
+esac
+make install
---
+++ 5_6.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+make mrproper
+make headers_check
+make INSTALL_HDR_PATH=dest headers_install
+cp -rv dest/include/* /tools/include
